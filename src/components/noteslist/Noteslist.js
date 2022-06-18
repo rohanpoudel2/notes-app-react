411d97c81f20a01097b8013357c3aa97ea0ae331
Rohan Poudel <nsrapoudel@gmail.com>
2022-06-18 14:42:40 +0545

    Added code to github

--- src/components/noteslist/Noteslist.js
+++ src/components/noteslist/Noteslist.js
@@ -1,12 +1,14 @@
 import Notes from "../notes/Notes"
 import Addnotes from "../addnotes/Addnotes";
 
+import { nanoid } from 'nanoid';
+
 import './noteslist.css';
 
 const Noteslist = ({ notesdata, adddata, deletedata }) => {
   return (
     <div className="noteslist">
-      {notesdata.map((notes) => <Notes id={notes.id} text={notes.text} date={notes.date} deletedata={deletedata} />)}
+      {notesdata.map((notes) => <Notes key={nanoid()} id={notes.id} text={notes.text} date={notes.date} deletedata={deletedata} />)}
       <Addnotes adddata={adddata} />
     </div>
   )
